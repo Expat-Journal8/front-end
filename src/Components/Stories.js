8799f7a98b8da43b0159f9d7c5acb9121a8b998f
rockyFierro <finnslighthouse@gmail.com>
2020-08-26 18:56:08 -0700

    :)

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -6,6 +6,8 @@ const EditStories =()=>{
     const  [story, setStory ] = useState( { title:"",trip:"" , date:"" } );
     const [entries, setEntries] = useState( [{ title:"",trip:"" , date:"" }] );
     const [editEntry, setEditEntry] = useState({title:"",trip:"",date:""});
+    const [editing, setEditing]=useState(false);
+
 
     const handleChange = e => {
         const tale = {
@@ -21,18 +23,14 @@ const EditStories =()=>{
         setStory({title:"", trip:"", date:""});    
     };
     
+   
+
     useEffect( ()=>{
         console.log("Story added to entries array: ",entries)
     
     },[entries] );
 
-    useEffect( ()=>{
-        console.log("Pulling story from array of entries with index from edit button event: ",entries[editEntry.index]); 
-        console.log("index given from edit button event: ",editEntry.index)
-
-    },[editEntry] );
-
-    const Journal =({entries,edit})=>{
+    const Journal =({entries})=>{
             return (
                        <div className="journalWrap">
                            {entries.map( (entry,index) => {
@@ -40,29 +38,57 @@ const EditStories =()=>{
                                <div key={index} className="wrapper">
                                <h3>{entry.title}</h3><span><h4>{entry.date}</h4></span>
                                <p>{entry.trip}</p>
-                               {index === 0 ?  null : <button onClick={() => setEditEntry({entry,index})}>Edit</button>}
+                               {index === 0 ?  null : <button onClick={() => {
+                                   setEditEntry({entry,index});
+                                   setEditing(true);
+                                }
+                               }>Edit</button>}
                                </div>)
                            } )}
                        </div>)
     };
+    
+    console.log(editing);
+    useEffect( ()=>{
+        console.log("Edit this: ",editEntry)
+        
+    },[editEntry] );
+    
     return(
         <div className="storiesWrapper">
             <form onSubmit={ handleSubmit }>
                 <div className="storyTitle">
-                    <label htmlFor="title">Title: </label>
-                    <input type="text" id="title" name="title" value={story.title} onChange={handleChange}/>
+                    <label 
+                    htmlFor="title">Title: </label>
+                    <input
+                     type="text" 
+                     id="title" 
+                     name="title" 
+                     value={ story.title} 
+                     onChange={handleChange}/>
                 </div>
                 <div className="story">
-                    <label htmlFor="editStory">Tell your story: </label>
-                    <textarea name="textarea" id="editStory" name="trip" value={story.trip} onChange={handleChange}/>
+                    <label 
+                    htmlFor="editStory">Tell your story: </label>
+                    <textarea 
+                    name="textarea" 
+                    id="editStory" 
+                    name="trip" 
+                    value={story.trip} 
+                    onChange={handleChange}/>
                 </div>
                 <div className="storyDate">
-                    <label htmlFor="date">Date: </label>
-                    <input type="text" id="date" name="date" value={story.date} onChange={handleChange}/>
+                    <label 
+                    htmlFor="date">Date: </label>
+                    <input 
+                    type="text" 
+                    id="date" 
+                    name="date" 
+                    value={story.date} 
+                    onChange={handleChange}/>
                 </div>
                 <button>Add Story</button>
             </form>
-
 
             <Journal entries={entries}/>
 
